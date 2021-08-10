d96b4f30739ccd0a1eb707ae877294c86074e9b8
ANrajin <an.rajin@gmail.com>
2021-08-11 01:08:05 +0600

    task one complete

--- Assignment2/Tasks/Task1/Program.cs
+++ Assignment2/Tasks/Task1/Program.cs
@@ -8,11 +8,35 @@ namespace Task1
         {
             var result = IsPrime(19);
             Console.WriteLine(result);
+
+            Console.WriteLine(IsPrime(15));
+            Console.WriteLine(IsPrime(5));
+            Console.WriteLine(IsPrime(12));
+            Console.WriteLine(IsPrime(23));
+            Console.WriteLine(IsPrime(20));
+            Console.WriteLine(IsPrime(33));
+            Console.WriteLine(IsPrime(133));
         }
 
         public static bool IsPrime(int number)
         {
-            throw new NotImplementedException();
+            if (number >= 2)
+            {
+
+                for (var i = 2; i <= number/2; i++)
+                {
+                    if(number % i == 0)
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
