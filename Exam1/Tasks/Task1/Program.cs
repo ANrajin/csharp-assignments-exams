82872342146e1adc13714ea841d2f006c2eca9aa
ANrajin <an.rajin@gmail.com>
2021-08-19 21:59:21 +0600

    task one complete

---
+++ Exam1/Tasks/Task1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Task1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = ConvertTime("12:30 pm");
+            Console.WriteLine(result);
+
+            //DateTime dt = DateTime.Parse("12:30 pm");
+
+            //DateTime dt = DateTime.Parse("1:30 pm");
+
+            //DateTime dt = DateTime.Parse("8:30 am");
+
+            //Console.WriteLine(dt);
+
+            //string convertedDt = dt.ToString("H:MM");
+            //convertedDt = dt.ToString("HH:mm");
+            //convertedDt = dt.ToString("H:mm");
+
+            //Console.WriteLine(convertedDt);
+        }
+
+        public static string ConvertTime(string twelveHourFormat)
+        {
+            DateTime dt = DateTime.Parse(twelveHourFormat);
+            string convertedDT = dt.ToString("H:mm");
+
+            return convertedDT;
+        }
+    }
+}
---
+++ Exam1/Tasks/Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Task2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //var result = Rotate("aaabbcc", 2);
+            //Console.WriteLine(result);
+
+            var str = "aaabbcc";
+
+            var 
+
+            Console.WriteLine(str.Remove(str.Length-1));
+        }
+
+        public static string Rotate(string original, int count)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
