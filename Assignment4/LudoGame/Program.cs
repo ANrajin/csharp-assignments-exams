53170a9d5fb80b3af15a1ec37d3c0d194ac6c70f
ANrajin <an.rajin@gmail.com>
2021-09-20 23:25:55 +0600

    updated

--- Assignment4/LudoGame/Program.cs
+++ Assignment4/LudoGame/Program.cs
@@ -1,6 +1,4 @@
-﻿using LudoGame.Players;
-using System;
-using System.Collections.Generic;
+﻿using System;
 
 namespace LudoGame
 {
@@ -8,26 +6,7 @@ namespace LudoGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("How Many Players?");
-            int n = Convert.ToInt32(Console.ReadLine());
-
-            List<Player> player = new List<Player>();
-
-            string[] colors = new string[]
-            {
-                "Red",
-                "Green",
-                "Blue",
-                "Yellow"
-            };
-
-            for (var i = 1;  i <= n; i++)
-            {
-                Console.WriteLine($"Enter player{i} name:");
-
-                var name = Console.ReadLine();
-                player.Add(new Player(name, colors[i-1]));
-            }
+            Console.WriteLine("Hello World");
         }
     }
 }
