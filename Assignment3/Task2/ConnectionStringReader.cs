fd9d6c130a0ef2c64b93d8bcb636ec196e88f978
ANrajin <an.rajin@gmail.com>
2021-09-22 20:31:52 +0600

    Task2 Dbcontext and entity class added

---
+++ Assignment3/Task2/ConnectionStringReader.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    public class ConnectionStringReader
+    {
+        public const string connectionString = "Server=.\\SQLEXPRESS; Database= TestDB; Trusted_Connection=True;";
+    }
+}
---
+++ Assignment3/Task2/Dbcontext/TrainingContext.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Task2.Dbcontext
+{
+    public class TrainingContext:DbContext
+    {
+        private readonly string _connectionString;
+        private readonly string _assemblyName;
+
+        public TrainingContext()
+        {
+            _connectionString = ConnectionStringReader.connectionString;
+            _assemblyName = typeof(Program).Assembly.FullName;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            if (!builder.IsConfigured)
+            {
+                builder.UseSqlServer(
+                    _connectionString,
+                    m => m.MigrationsAssembly(_assemblyName)
+                );
+
+                base.OnConfiguring(builder);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
